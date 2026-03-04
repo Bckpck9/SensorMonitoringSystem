c18248209c797a917eea0dfa51128620e21ce753
Denisenko Bogdan <denisenkobogdan954@gmail.com>
2026-03-04 17:06:12 +0700

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
-const API = '/api;
+const API = '/api';
 
 const Home = () => {
   const [sensors, setSensors] = useState([]);
