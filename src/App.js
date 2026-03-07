ce9899666ab3197b8700e68f605463aceb9d19fd
Denisenko Bogdan <denisenkobogdan954@gmail.com>
2026-03-07 15:02:59 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import Home from './pages/Home';
 import Detail from './pages/Detail';
 import Form from './pages/Form';
 
+import IncidentsHome from './pages/IncidentsHome';
+import IncidentDetail from './pages/IncidentDetail';
+import IncidentForm from './pages/IncidentForm';
+
 export default function App() {
   return (
     <Router>
@@ -11,6 +15,11 @@ export default function App() {
         <Route path="/detail/:id" element={<Detail />} />
         <Route path="/add" element={<Form mode="create" />} />
         <Route path="/edit/:id" element={<Form mode="edit" />} />
+
+        <Route path="/incidents" element={<IncidentsHome />} />
+        <Route path="/incidents/add" element={<IncidentForm mode="create" />} />
+        <Route path="/incidents/edit/:id" element={<IncidentForm mode="edit" />} />
+        <Route path="/incidents/:id" element={<IncidentDetail />} />
       </Routes>
     </Router>
   );
